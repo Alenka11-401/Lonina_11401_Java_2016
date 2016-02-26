77e0558aaceba354fb462aba15534d7f564775d1
Alenka11-401 <alenka.lonina@gmail.com>
2016-02-26 11:16:48 +0300

    Added files via upload

---
+++ Institute.java
@@ -0,0 +1,16 @@
+/**
+ * Created by alena on 26.02.16.
+ */
+public class Institute {
+    //comments
+    private String name;
+    public Institute(String name){
+        this.name = name;
+    }
+    String getInstitute(){
+        return this.name;
+    }
+    void setInstitute(String name){
+        this.name = name;
+    }
+}
